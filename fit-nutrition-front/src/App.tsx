b97a4d71f9badcb9e29da1555ce8ae498a6220a9
Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
2024-12-05 18:06:59 +0000

    Namechange, profile is gone, fig is gone

--- fit-nutrition-front/src/App.tsx
+++ fit-nutrition-front/src/App.tsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignIn from "./pages/SignIn";
-import Profile from "./pages/Profile";
 import PatientProfile from "./pages/PatientProfile";
 import RegisterPage from "./pages/Register";
 
@@ -14,7 +13,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/profile" element={<Profile />} />
           <Route path="/patient-profile" element={<PatientProfile />} />
         </Routes>
       </Router>
