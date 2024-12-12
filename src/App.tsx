efd18d36fdd3b936960d0c5cf90e39a65b63d2d1
Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
2024-12-12 19:12:01 +0000

    Dashboard added (#45)

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import RegisterPage from "./pages/Register";
 import LoginPage from "./pages/Login";
 import LandingPage from "./pages/LandingPage";
 import WIPPage from "./pages/WIPPage";
+import DietitianDashboard from "./pages/DietitianDashboard";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/recipes" element={<Recipes />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/patient-profile" element={<PatientProfile />} />
+          <Route path="/dietitian-dashboard" element={<DietitianDashboard />} />
         </Routes>
       </Router>
     </div>
